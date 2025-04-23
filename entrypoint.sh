737b3330967f0928813e50991392f5e675ec5472
Chris Aubuchon <aubuchcl@gmail.com>
2025-04-23 14:33:49 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,19 +11,19 @@ echo "Attempting to resolve $TARGET..."
 failure_start=""
 
 while true; do
-    if getent hosts "$TARGET" > /dev/null 2>&1; then
+    if ping -c 1 -W 1 "$TARGET" > /dev/null 2>&1; then
         echo "$TARGET is reachable!"
         failure_start=""
     else
         now=$(date +%s)
-        echo "Could not resolve other container ($TARGET), retrying..."
+        echo "Could not reach other container ($TARGET), retrying..."
 
         if [ -z "$failure_start" ]; then
             failure_start=$now
         else
             elapsed=$(( now - failure_start ))
             if [ "$elapsed" -ge "$timeout" ]; then
-                echo "Could not resolve other container ($TARGET) for $timeout seconds"
+                echo "Could not reach other container ($TARGET) for $timeout seconds"
                 exit 1
             fi
         fi
@@ -31,3 +31,4 @@ while true; do
 
     sleep 1
 done
+
