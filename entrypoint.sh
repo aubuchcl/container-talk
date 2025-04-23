3cbebf360e9f5f7ac5030f4365919577498dca55
Chris Aubuchon <aubuchcl@gmail.com>
2025-04-23 15:49:13 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,12 +11,17 @@ echo "Attempting to resolve $TARGET..."
 failure_start=""
 
 while true; do
-    if ping -c 1 -W 1 "$TARGET" > /dev/null 2>&1; then
+    # Capture output and status code
+    output=$(ping -c 1 -W 1 "$TARGET" 2>&1)
+    status=$?
+
+    if [ $status -eq 0 ]; then
         echo "$TARGET is reachable!"
         failure_start=""
     else
         now=$(date +%s)
         echo "Could not reach other container ($TARGET), retrying..."
+        echo "Ping output: $output"
 
         if [ -z "$failure_start" ]; then
             failure_start=$now
@@ -31,4 +36,3 @@ while true; do
 
     sleep 1
 done
-
